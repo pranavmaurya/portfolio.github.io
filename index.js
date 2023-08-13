1251c29e0868901fe8cc5055a33329777b4dc5a6
Pranav Maurya <85123163+pranavmaurya@users.noreply.github.com>
2023-08-13 13:58:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//certificates
+// document.addEventListener('DOMContentLoaded', function() {
+//     const certificateImages = document.querySelectorAll('.certificate-image');
+    
+//     certificateImages.forEach(function(image) {
+//       const src = image.getAttribute('data-src');
+//       image.setAttribute('src', src);
+//     });
+//   });
+
+  
